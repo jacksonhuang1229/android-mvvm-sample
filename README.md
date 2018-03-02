add45c2c73c4e203e522d7231e07d460e18cbbda
jacksonh <jacksonh@synology.com>
2018-03-02 19:58:40 +0800

    a basic sample for start

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # android-mvvm-sample
 show some sample for using mvvm structure in android
+
+# reference
+https://developer.android.com/topic/libraries/data-binding/index.html
---
+++ app/src/main/java/com/example/jacksonh/android_mvvm_sample/User.java
@@ -0,0 +1,15 @@
+package com.example.jacksonh.android_mvvm_sample;
+
+/**
+ * Created by jacksonh on 2018/3/2.
+ */
+
+public class User {
+    public final String firstName;
+    public final String lastName;
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+}
